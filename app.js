606d476064f5819ab85353b255c2e05d81e604bd
Christophe Geers <geersch@gmail.com>
2014-07-25 01:03:36 +0200

    Added demo page

---
+++ app.js
@@ -0,0 +1,9 @@
+var demo = angular.module('demo', ['ui.cg.numberinput']);
+
+demo.controller('DemoCtrl', ['$scope', function($scope) {
+  
+}]);
+
+demo.controller('NumberInputCtrl', ['$scope', function ($scope) {
+	$scope.value = 34.75;
+}]);
