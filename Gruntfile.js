485082e1e99d2eb0f9286024a4a8a96e7e080fdb
Christophe Geers <geersch@gmail.com>
2014-07-27 21:10:52 +0200

    grunt jsbeautifier: included the Gruntfile.js itself

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,55 +1,56 @@
 module.exports = function (grunt) {
 
-	grunt.loadNpmTasks('grunt-contrib-concat');
-	grunt.loadNpmTasks('grunt-contrib-uglify');
-	grunt.loadNpmTasks('grunt-contrib-clean');
-	grunt.loadNpmTasks('grunt-contrib-connect');
-	grunt.loadNpmTasks('grunt-ngdocs');	
-	grunt.loadNpmTasks('grunt-open');
+    grunt.loadNpmTasks('grunt-contrib-concat');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-connect');
+    grunt.loadNpmTasks('grunt-ngdocs');
+    grunt.loadNpmTasks('grunt-open');
     grunt.loadNpmTasks('grunt-gh-pages');
     grunt.loadNpmTasks('grunt-karma');
     grunt.loadNpmTasks('grunt-jsbeautifier');
 
-	grunt.initConfig({
-		modules: [], // filled in by the build task
-		dist: 'dist',
-		filename: 'ui-cg',
-		pkg: grunt.file.readJSON('package.json'),
-		meta: {
-			modules: 'angular.module("ui.cg", [<%= srcModules %>]);',
-			banner: ['/*',
-					 ' * <%= pkg.name %>',
-					 ' * <%= pkg.homepage %>',
-					 ' * Version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>',
-					 ' * License: <%= pkg.license %>',
-					 ' */\n'].join('\n')
-		},
-		concat: {
+    grunt.initConfig({
+        modules: [], // filled in by the build task
+        dist: 'dist',
+        filename: 'ui-cg',
+        pkg: grunt.file.readJSON('package.json'),
+        meta: {
+            modules: 'angular.module("ui.cg", [<%= srcModules %>]);',
+            banner: ['/*',
+                ' * <%= pkg.name %>',
+                ' * <%= pkg.homepage %>',
+                ' * Version: <%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>',
+                ' * License: <%= pkg.license %>',
+                ' */\n'
+            ].join('\n')
+        },
+        concat: {
             options: {
                 banner: '<%= meta.banner %><%= meta.modules %>\n'
             },
-			dist: {
-				src: [], // filled in by the build task
-				dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.js'
-			}
-		},
-		uglify: {
-			options: {
-				banner: '<%= meta.banner %>'
-			},
-			dist: {
-				src: ['<%= concat.dist.dest %>'],
-				dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.min.js'
-			}
-		},
-		clean: {
-			all: ['<%= dist %>'],
-			docs: ['<%= ngdocs.options.dest %>']
-		},
-		ngdocs: {
-			options: {
-				dest: 'dist/docs',
-				scripts: [
+            dist: {
+                src: [], // filled in by the build task
+                dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.js'
+            }
+        },
+        uglify: {
+            options: {
+                banner: '<%= meta.banner %>'
+            },
+            dist: {
+                src: ['<%= concat.dist.dest %>'],
+                dest: '<%= dist %>/<%= filename %>-<%= pkg.version %>.min.js'
+            }
+        },
+        clean: {
+            all: ['<%= dist %>'],
+            docs: ['<%= ngdocs.options.dest %>']
+        },
+        ngdocs: {
+            options: {
+                dest: 'dist/docs',
+                scripts: [
                     'angular.js',
                     '<%= concat.dist.dest %>'
                 ],
@@ -58,27 +59,27 @@ module.exports = function (grunt) {
                 ],
                 navTemplate: 'docs/nav.html',
                 title: 'ui.cg',
-				html5Mode: false
-			},
+                html5Mode: false
+            },
             api: {
                 src: ['<%= concat.dist.dest %>', 'docs/index.ngdoc'],
                 title: 'API Documentation'
             }
-		},
-		connect: {
-			options: {
-				hostname: 'localhost',
-				base: '<%= ngdocs.options.dest %>',
-				keepalive: true
-			},
-			server: {}
-		},
-		open: {
-			docs: {
-				path: 'http://localhost:8000',
-				app: 'Chrome'
-			}
-		},
+        },
+        connect: {
+            options: {
+                hostname: 'localhost',
+                base: '<%= ngdocs.options.dest %>',
+                keepalive: true
+            },
+            server: {}
+        },
+        open: {
+            docs: {
+                path: 'http://localhost:8000',
+                app: 'Chrome'
+            }
+        },
         'gh-pages': {
             options: {
                 base: '<%= ngdocs.options.dest %>'
@@ -94,7 +95,7 @@ module.exports = function (grunt) {
             }
         },
         jsbeautifier: {
-            files: ['src/**/*.js'],
+            files: ['Gruntfile.js', 'src/**/*.js'],
             options: {
                 js: {
                     indentSize: 4,
@@ -103,40 +104,41 @@ module.exports = function (grunt) {
                 }
             }
         }
-	});
+    });
 
-	var foundModules = {};
-	function findModule(name) {
-		if (foundModules[name]) {
-			return; 
-		}
-		
-		function breakup(text, separator) {
-			return text.replace(/[A-Z]/g, function (match) {
-				return separator + match;
-			});
-		}
-		
-		function ucwords(text) {
-			return text.replace(/^([a-z])|\s+([a-z])/g, function ($1) {
-				return $1.toUpperCase();
-			});
-		}
-		
-		function enquote(value) {
-			return '"' + value + '"';
-		}
-		
-		var module = {
-			name: name,
-			moduleName: enquote('ui.cg.' + name),
-			displayName: ucwords(breakup(name, '')),
-			srcFiles: grunt.file.expand('src/' + name + '/*.js'),
+    var foundModules = {};
+
+    function findModule(name) {
+        if (foundModules[name]) {
+            return;
+        }
+
+        function breakup(text, separator) {
+            return text.replace(/[A-Z]/g, function (match) {
+                return separator + match;
+            });
+        }
+
+        function ucwords(text) {
+            return text.replace(/^([a-z])|\s+([a-z])/g, function ($1) {
+                return $1.toUpperCase();
+            });
+        }
+
+        function enquote(value) {
+            return '"' + value + '"';
+        }
+
+        var module = {
+            name: name,
+            moduleName: enquote('ui.cg.' + name),
+            displayName: ucwords(breakup(name, '')),
+            srcFiles: grunt.file.expand('src/' + name + '/*.js'),
             ngdocs: grunt.file.expand('src/' + name + '/docs/*.ngdoc')
-		};
+        };
 
-		grunt.config('modules', grunt.config('modules').concat(module));
-	}
+        grunt.config('modules', grunt.config('modules').concat(module));
+    }
 
     function getFilename(path) {
         var index = path.lastIndexOf('/');
@@ -146,20 +148,21 @@ module.exports = function (grunt) {
 
         return filename;
     }
-	
-	grunt.registerTask('build', 'Build the distributable', function () {
-		var _ = grunt.util._;
-		
-		// Search all modules inside of the src/ directory
-		grunt.file.expand({
-			filter: 'isDirectory', cwd: '.'
-		}, 'src/*').forEach(function (dir) {
-			findModule(dir.split('/')[1]);
-		});
-		
-		var modules = grunt.config('modules');
-		
-		grunt.config('srcModules', _.pluck(modules, 'moduleName'));
+
+    grunt.registerTask('build', 'Build the distributable', function () {
+        var _ = grunt.util._;
+
+        // Search all modules inside of the src/ directory
+        grunt.file.expand({
+            filter: 'isDirectory',
+            cwd: '.'
+        }, 'src/*').forEach(function (dir) {
+            findModule(dir.split('/')[1]);
+        });
+
+        var modules = grunt.config('modules');
+
+        grunt.config('srcModules', _.pluck(modules, 'moduleName'));
 
         var srcFiles = [];
         _.each(modules, function (module) {
@@ -176,23 +179,23 @@ module.exports = function (grunt) {
             });
         });
 
-		// Set the concat task to concatenate the given src modules
-		grunt.config(
-			'concat.dist.src',
-			grunt.config('concat.dist.src').concat(srcFiles));
+        // Set the concat task to concatenate the given src modules
+        grunt.config(
+            'concat.dist.src',
+            grunt.config('concat.dist.src').concat(srcFiles));
 
-		grunt.task.run(['test', 'clean', 'concat', 'uglify']);
-	});
+        grunt.task.run(['test', 'clean', 'concat', 'uglify']);
+    });
 
     grunt.registerTask('test', 'Run the tests (single-run)', ['karma']);
-	
-	grunt.registerTask('show-docs', 'Open the API docs', function () {
-		grunt.task.run(['build', 'ngdocs', 'open:docs', 'connect']);
-	});
-	
-	grunt.registerTask('default', ['show-docs']);
+
+    grunt.registerTask('show-docs', 'Open the API docs', function () {
+        grunt.task.run(['build', 'ngdocs', 'open:docs', 'connect']);
+    });
+
+    grunt.registerTask('default', ['show-docs']);
 
     grunt.registerTask('publish-docs', ['build', 'ngdocs', 'gh-pages']);
 
-	return grunt;
+    return grunt;
 }
