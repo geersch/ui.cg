e33d419169f3475e52c3b966d36f480e15c197c9
Christophe Geers <geersch@gmail.com>
2014-07-27 09:48:29 +0200

    Updates

---
+++ .grunt/grunt-gh-pages/gh-pages/src/js/docs-setup.js
@@ -0,0 +1,34 @@
+NG_DOCS={
+  "sections": {
+    "api": "API Documentation"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "index",
+      "shortName": "Overview",
+      "type": "overview",
+      "moduleName": "Overview",
+      "shortDescription": "Input controls for your Angular apps, made easy",
+      "keywords": "add adipiscing aliquet amet angular angularjs api apps blandit bring build built cg clone configurable consectetur controls decimal decimals dependencies dictum dist dolor easiest easy easy-to-use elit enim erat eros faucibus fermentum feugiat find floating folder fringilla geers git grunt imperdiet input install ipsum javascript justo lorem module mymodule nec nisl nmp node npm nulla number numberinput org overview point porttitor quick quis quisque redistributables repository rutrum separator sit start tempus turpis ui ultrices uncompressed ut vel vestibulum vivamus"
+    },
+    {
+      "section": "api",
+      "id": "cg.ui.directive:numberinput",
+      "shortName": "numberinput",
+      "type": "directive",
+      "moduleName": "cg.ui",
+      "shortDescription": "Input for a floating point number with a configurable decimal separator (default: .) and decimals (default: 2).",
+      "keywords": "$scope allowed angular api app assignable cg configurable controller data-bind decimal decimal-separator decimals directive display expression floating function html input js maximum module ng-controller ng-model ngmodel number numberinput numberinputctrl point separator ui var"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js",
+    "ui-cg-0.1.0-BETA.js"
+  ]
+};
