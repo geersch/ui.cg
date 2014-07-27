92a9632f1e9c681bff2e0a24715bd25a998d6f5b
Christophe Geers <geersch@gmail.com>
2014-07-27 10:06:39 +0200

    Removed old demo files

--- app.js
+++
@@ -1,9 +0,0 @@
-var demo = angular.module('demo', ['ui.cg']);
-
-demo.controller('DemoCtrl', ['$scope', function($scope) {
-  
-}]);
-
-demo.controller('NumberInputCtrl', ['$scope', function ($scope) {
-	$scope.value = 34.75;
-}]);
