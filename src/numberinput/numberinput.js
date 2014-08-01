ede236b55dd82f8775bfd6e774c7c6cbaa1c895d
Christophe Geers <geersch@gmail.com>
2014-08-01 08:42:03 +0200

    Refactored the numberinput directive

--- src/numberinput/numberinput.js
+++ src/numberinput/numberinput.js
@@ -1,45 +1,157 @@
 angular.module('ui.cg.numberinput', [])
 
-.directive('numberinput', function () {
+.constant('numberinputConfig', {
+    decimalSeparator: '.',
+    decimals: 2,
+    step: 1
+})
 
+.controller('NumberInputController', ['$scope', '$attrs', 'numberinputConfig', function ($scope, $attrs, numberinputConfig) {
     var HOT_KEYS = [38, 40];
 
+    this.init = function (element) {
+        var input = element.find('input').eq(0);
+        this.bindKeyboardEvents(input);
+        this.bindMouseWheelEvents(input);
+    };
+
+    function stepIt(step) {
+        var modelValue = $scope.number;
+        if (isNaN(modelValue)) {
+            modelValue = 0;
+        }
+
+        var multiplier = decimals === 0 ? 1 : parseInt('1' + Array(decimals + 1).join("0"), 10);
+        var modelValue = Math.round((modelValue + step) * multiplier) / multiplier;
+
+        $scope.number = modelValue
+    }
+
+    $scope.increment = function () {
+        var step = getStep();
+        stepIt(step);
+    }
+
+    $scope.decrement = function () {
+        var step = getStep();
+        stepIt(step * -1);
+    }
+
+    this.bindMouseWheelEvents = function(input) {
+        var isScrollingUp = function (e) {
+            if (e.originalEvent) {
+                e = e.originalEvent;
+            }
+
+            var delta = (e.wheelDelta) ? e.wheelDelta : -e.deltaY;
+
+            return (e.detail || delta > 0);
+        };
+
+        input.bind('mousewheel wheel', function (e) {
+            $scope.$apply(isScrollingUp(e) ? $scope.increment() : $scope.decrement());
+            e.preventDefault();
+        });
+    }
+
+    this.bindKeyboardEvents = function(input) {
+        input.bind('keydown', function (e) {
+            if (HOT_KEYS.indexOf(e.which) === -1) {
+                return;
+            }
+
+            e.stopPropagation();
+
+            if (e.which === 40) {
+                $scope.decrement();
+            } else if (e.which === 38) {
+                $scope.increment();
+            }
+
+            $scope.$apply();
+        });
+    };
+
+    this.getDecimalSeparator = function () {
+        return decimalSeparator;
+    }
+
+    var decimalSeparator = numberinputConfig.decimalSeparator;
+    if ($attrs.decimalSeparator) {
+        $attrs.$observe('decimalSeparator', function (value) {
+            var parsed;
+            if (!angular.isDefined(value) || value.length === 0) {
+                parsed = numberinputConfig.decimalSeparator;
+            } else {
+                parsed = value[0];
+            }
+
+            decimalSeparator = parsed;
+        })
+    }
+
+    this.getDecimals = function () {
+        return decimals;
+    }
+
+    var decimals = numberinputConfig.decimals;
+    if ($attrs.decimals) {
+        $attrs.$observe('decimals', function (value) {
+            var parsed;
+            if (angular.isUndefined(value) || isNaN(value)) {
+                parsed = numberinputConfig.decimals;
+            } else {
+                parsed = parseInt(value, 10);
+            }
+
+            if (parsed < 0) {
+                parsed = 0;
+            }
+
+            decimals = parsed;
+        });
+    }
+
+    function getStep() {
+        var corrected = step;
+        var minimumStep = decimals === 0 ? 1 : parseFloat('0.' + Array(decimals).join("0") + "1", 10);
+        if (corrected < minimumStep) {
+            corrected = minimumStep;
+        }
+
+        return corrected;
+    }
+
+    var step = numberinputConfig.step;
+    if ($attrs.step) {
+        $attrs.$observe('step', function (value) {
+            var parsed;
+            if (angular.isUndefined(value) || isNaN(value)) {
+                parsed = numberinputConfig.step;
+            } else {
+                parsed = parseFloat(value, 10);
+            }
+
+            step = parsed;
+        });
+    }
+}])
+
+.directive('float', function () {
     return {
-        restrict: 'E',
-        require: 'ngModel',
-        templateUrl: 'template/numberinput/numberinput.html',
-        replace: true,
-        link: function (scope, element, attrs, ctrl) {
-            var decimalSeparator = '.';
-            var decimals = 2;
-
-            // Determine the decimal separator
-            if (angular.isDefined(attrs.decimalSeparator) && attrs.decimalSeparator.length > 0) {
-                decimalSeparator = attrs.decimalSeparator[0];
-            }
-
-            // Determine the number of decimals
-            if (angular.isDefined(attrs.decimals)) {
-                var value = parseInt(attrs.decimals, 10);
-                if (!isNaN(value)) {
-                    decimals = value;
-                }
-
-                if (decimals < 0) {
-                    decimals = 0;
-                }
-            }
-
-            var step = scope.$eval(attrs.step) || 1;
-            var minimumStep = decimals === 0 ? 1 : parseFloat('0.' + Array(decimals).join("0") + "1", 10);
-            if (step < minimumStep) {
-                step = minimumStep;
-            }
+        restrict: 'A',
+        require: ['^numberinput', '^ngModel'],
+        link: function (scope, element, attrs, ctrls) {
+            var numberinputCtrl = ctrls[0],
+                ngModelCtrl = ctrls[1];
 
             function sanitizeFloat(input) {
                 function clean(input) {
                     return input.replace(/[^-0-9]/g, '');
                 }
+
+                var decimalSeparator = numberinputCtrl.getDecimalSeparator();
+                var decimals = numberinputCtrl.getDecimals();
 
                 var sanitized = String(input).replace('.', decimalSeparator);
 
@@ -71,29 +183,12 @@ angular.module('ui.cg.numberinput', [])
                     return undefined;
                 }
 
-                return parseFloat(input.replace(decimalSeparator, '.'));
+                var decimalSeparator = numberinputCtrl.getDecimalSeparator();
+
+                return parseFloat(input.replace(decimalSeparator, '.'), 10);
             }
-
-            function addToModelValue(step) {
-                return function () {
-                    var modelValue = ctrl.$modelValue;
-                    if (isNaN(modelValue)) {
-                        modelValue = 0;
-                    }
-
-                    var multiplier = decimals === 0 ? 1 : parseInt('1' + Array(decimals + 1).join("0"), 10);
-                    var modelValue = Math.round((modelValue + step) * multiplier) / multiplier;
-
-                    ctrl.$setViewValue(modelValue);
-                    ctrl.$render();
-                }
-            }
-
-            var incrementByStep = addToModelValue(step);
-            var decrementByStep = addToModelValue(step * -1);
 
             function sanitize(input) {
-
                 if (angular.isUndefined(input) || input.length === 0) {
                     return undefined;
                 }
@@ -102,63 +197,23 @@ angular.module('ui.cg.numberinput', [])
                 var modelValue = convertToFloat(viewValue);
 
                 if (viewValue !== input) {
-                    ctrl.$setViewValue(viewValue);
-                    ctrl.$render();
+                    ngModelCtrl.$setViewValue(viewValue);
+                    ngModelCtrl.$render();
                 }
 
                 return modelValue;
             }
 
-            ctrl.$parsers.unshift(sanitize);
+            ngModelCtrl.$parsers.unshift(sanitize);
+            ngModelCtrl.$formatters.unshift(sanitize);
 
-            ctrl.$formatters.unshift(sanitize);
-
-            ctrl.$formatters.unshift(function (input) {
+            ngModelCtrl.$formatters.unshift(function (input) {
                 if (angular.isUndefined(input)) {
-                    return undefined;
+                    return '';
                 }
 
+                var decimalSeparator = numberinputCtrl.getDecimalSeparator();
                 return String(input).replace('.', decimalSeparator);
-            });
-
-            //bind keyboard events: arrows up(38) / down(40)
-            element.bind('keydown', function (evt) {
-                if (HOT_KEYS.indexOf(evt.which) === -1) {
-                    return;
-                }
-
-                evt.stopPropagation();
-
-                if (evt.which === 40) {
-                    decrementByStep();
-                } else if (evt.which === 38) {
-                    incrementByStep();
-                }
-
-                scope.$apply();
-            });
-
-            element.bind('mousewheel wheel', function(evt) {
-
-                var isScrollingUp = function (e) {
-                    if (e.originalEvent) {
-                        e = e.originalEvent;
-                    }
-
-                    var delta = (e.wheelDelta) ? e.wheelDelta : -e.deltaY;
-                    return (e.detail || delta > 0);
-                };
-
-                evt.preventDefault();
-
-                if (isScrollingUp(evt)) {
-                    incrementByStep();
-                }
-                else {
-                    decrementByStep();
-                }
-
-                scope.$apply();
             });
 
             element.bind('blur', function () {
@@ -167,6 +222,9 @@ angular.module('ui.cg.numberinput', [])
                     return;
                 }
 
+                var decimalSeparator = numberinputCtrl.getDecimalSeparator();
+                var decimals = numberinputCtrl.getDecimals();
+
                 value = parseFloat(value.replace(decimalSeparator, '.'));
                 var rounded = value.toFixed(decimals).replace('.', decimalSeparator);
                 if (rounded !== value) {
@@ -174,6 +232,25 @@ angular.module('ui.cg.numberinput', [])
                 }
             });
         }
-    };
+    }
+})
 
+.directive('numberinput', function () {
+    return {
+        restrict: 'E',
+        require: ['numberinput'],
+        controller: 'NumberInputController',
+        templateUrl: 'template/numberinput/numberinput.html',
+        replace: true,
+        scope: {
+            number: '=ngModel',
+            decimalSeparator: '@',
+            decimals: '@',
+            step: '@'
+        },
+        link: function (scope, element, attrs, ctrls) {
+            var numberinputCtrl = ctrls[0];
+            numberinputCtrl.init(element);
+        }
+    };
 });
--- src/numberinput/test/numberinput.spec.js
+++ src/numberinput/test/numberinput.spec.js
@@ -10,19 +10,30 @@ describe('numberinput', function () {
     beforeEach(inject (function ($sniffer) {
 
         changeInputValueTo = function (element, value) {
-            element.val(value);
-            element.trigger($sniffer.hasEvent('input') ? 'input' : 'change');
+            var input = element.find('input').eq(0);
+            input.val(value);
+            input.trigger($sniffer.hasEvent('input') ? 'input' : 'change');
             $scope.$digest();
         };
 
     }));
+
+    function getElementValue(element) {
+        var input = element.find('input').eq(0);
+        return input.val();
+    }
+
+    function blur(element) {
+        var input = element.find('input').eq(0);
+        input.trigger('blur');
+    }
 
     function createInput(html) {
         var element;
 
         inject(function ($rootScope, $compile) {
             $scope = $rootScope.$new();
-            $scope.input = 0;
+            $scope.input = 200;
             element = $compile(angular.element(html))($scope);
             $rootScope.$digest();
         });
@@ -33,88 +44,90 @@ describe('numberinput', function () {
     var triggerKeyDown = function (element, keyCode) {
         var e = $.Event('keydown');
         e.which = keyCode;
-        element.trigger(e);
+        var input = element.find('input').eq(0);
+        input.trigger(e);
     };
 
     function triggerMouseWheelEvent(element, delta) {
         var e = $.Event('mousewheel');
         e.wheelDelta = delta;
-        element.trigger(e);
+        var input = element.find('input').eq(0);
+        input.trigger(e);
     }
 
-    describe('basic functionality', function () {
+    describe('with default settings', function () {
         var element;
 
         beforeEach(function () {
             element = createInput('<numberinput ng-model="input" />');
         });
 
-        it('it should only accept numbers as input', function () {
+        it('should only accept numbers as input', function () {
             changeInputValueTo(element, '1a2b3c4d5e');
-            expect(element.val()).toEqual('12345');
+            expect(getElementValue(element)).toEqual('12345');
         });
 
-        it('it should accept a dot (.) as a decimal separator', function () {
+        it('should accept a dot (.) as a decimal separator', function () {
             changeInputValueTo(element, '1.25');
-            expect(element.val()).toEqual('1.25');
+            expect(getElementValue(element)).toEqual('1.25');
         });
 
-        it('it should not accept anything but a dot (.) as a decimal separator', function () {
+        it('should not accept anything but a dot (.) as a decimal separator', function () {
             changeInputValueTo(element, '1,25');
-            expect(element.val()).toEqual('125');
+            expect(getElementValue(element)).toEqual('125');
         });
 
-        it('it should only accept one decimal separator', function () {
+        it('should only accept one decimal separator', function () {
             changeInputValueTo(element, '10.1.0');
-            expect(element.val()).toEqual('10.10');
+            expect(getElementValue(element)).toEqual('10.10');
         });
 
-        it('it should only allow 2 decimals', function () {
+        it('should only allow 2 decimals', function () {
             changeInputValueTo(element, '1.236');
-            expect(element.val()).toEqual('1.23');
+            expect(getElementValue(element)).toEqual('1.23');
         });
 
-        it('it should be able to handle an empty value', function () {
+        it('should be able to handle an empty value', function () {
             changeInputValueTo(element, '');
-            expect(element.val()).toEqual('');
+            expect(getElementValue(element)).toEqual('');
         });
 
-        it('it should round to two decimals on blur', function () {
+        it('should round to two decimals on blur', function () {
             changeInputValueTo(element, '1.2');
-            element.trigger('blur');
-            expect(element.val()).toEqual('1.20');
+            blur(element);
+            expect(getElementValue(element)).toEqual('1.20');
         });
 
-        it('it should not allow a decimal separator to be entered as the first character', function () {
+        it('should not allow a decimal separator to be entered as the first character', function () {
             changeInputValueTo(element, '.');
-            expect(element.val()).toEqual('');
+            expect(getElementValue(element)).toEqual('');
         });
 
-        it('model value should be a float', function () {
+        it('should store the model value as a float', function () {
             changeInputValueTo(element, '1.23');
             expect($scope.input).toEqual(1.23);
             expect(typeof $scope.input).toEqual('number');
         });
 
-        it('view value should be a string', function () {
+        it('should store the view value as a string', function () {
             changeInputValueTo(element, '4.56');
-            expect(element.val()).toEqual('4.56');
-            expect(typeof element.val()).toEqual('string');
+            expect(getElementValue(element)).toEqual('4.56');
+            expect(typeof getElementValue(element)).toEqual('string');
         });
     });
 
-    describe('decimal separator', function () {
+    describe('decimal separator setting', function () {
 
-        it('it should accept a custom decimal separator', function () {
+        it('should accept a custom decimal separator', function () {
             var element = createInput( '<numberinput ng-model="input" decimal-separator="x" />');
             changeInputValueTo(element, '1.25');
-            expect(element.val()).toEqual('1x25');
+            expect(getElementValue(element)).toEqual('1x25');
         });
 
-        it('it should only use the first character of the decimal separator', function () {
-            var element = createInput('<numberinput ng-model="inpu" decimal-separator="abc" />');
+        it('should only use the first character of the decimal separator', function () {
+            var element = createInput('<numberinput ng-model="input" decimal-separator="abc" />');
             changeInputValueTo(element, '4.56');
-            expect(element.val()).toEqual('4a56');
+            expect(getElementValue(element)).toEqual('4a56');
         })
     });
 
@@ -123,19 +136,22 @@ describe('numberinput', function () {
         it('should not accept a decimal separator if the number of decimals is zero', function () {
             var element = createInput('<numberinput ng-model="input" decimals="0" />');
             changeInputValueTo(element, '14.25');
-            expect(element.val()).toEqual('14');
+            expect(getElementValue(element)).toEqual('14');
+            expect($scope.input).toEqual(14);
         });
 
         it('should treat a negative number for the decimals attribute as zero', function () {
             var element = createInput('<numberinput ng-model="input" decimals="-5" />');
             changeInputValueTo(element, '14.25');
-            expect(element.val()).toEqual('14');
+            expect(getElementValue(element)).toEqual('14');
+            expect($scope.input).toEqual(14);
         })
 
         it('should only allow 3 decimals', function () {
             var element = createInput('<numberinput ng-model="input" decimals="3" />');
             changeInputValueTo(element, '1.2345');
-            expect(element.val()).toEqual('1.234');
+            expect(getElementValue(element)).toEqual('1.234');
+            expect($scope.input).toEqual(1.234);
         });
 
         it('should have 3 decimals for the model value', function () {
@@ -148,11 +164,11 @@ describe('numberinput', function () {
             var element = createInput('<numberinput ng-model="input" decimals="foobar" />');
             changeInputValueTo(element, '1.2345');
             expect($scope.input).toEqual(1.23);
-            expect(element.val()).toEqual('1.23');
+            expect(getElementValue(element)).toEqual('1.23');
         });
     });
 
-    describe('should respond on keyboard navigation', function () {
+    describe('keyboard navigation', function () {
         var element;
 
         beforeEach(function () {
@@ -160,36 +176,36 @@ describe('numberinput', function () {
             element = createInput(html);
         });
 
-        it('by increasing the value when the up arrow is pressed', function () {
+        it('should increase the value when the up arrow is pressed', function () {
             changeInputValueTo(element, '1');
             triggerKeyDown(element, 38);
-            expect(element.val()).toEqual('2');
+            expect(getElementValue(element)).toEqual('2');
             expect($scope.input).toEqual(2);
         });
 
-        it('by decreasing the value when the down arrow is pressed', function () {
+        it('should decrease the value when the down arrow is pressed', function () {
             changeInputValueTo(element, '2');
             triggerKeyDown(element, 40);
-            expect(element.val()).toEqual('1');
+            expect(getElementValue(element)).toEqual('1');
             expect($scope.input).toEqual(1);
         });
 
-        it('by starting at zero when the up arrow is pressed for an empty value', function () {
+        it('should start at zero when the up arrow is pressed for an empty value', function () {
             changeInputValueTo(element, '');
             triggerKeyDown(element, 38);
-            expect(element.val()).toEqual('1');
+            expect(getElementValue(element)).toEqual('1');
             expect($scope.input).toEqual(1);
         });
 
-        it('by starting at zero when the down arrow is pressed for an empty value', function () {
+        it('shoult start at zero when the down arrow is pressed for an empty value', function () {
             changeInputValueTo(element, '');
             triggerKeyDown(element, 40);
-            expect(element.val()).toEqual('-1');
+            expect(getElementValue(element)).toEqual('-1');
             expect($scope.input).toEqual(-1);
         })
     });
 
-    describe('should respond on mousewheel events', function () {
+    describe('mouse navigation', function () {
         var element;
 
         beforeEach(function () {
@@ -197,39 +213,39 @@ describe('numberinput', function () {
             element = createInput(html);
         });
 
-        it('by increasing the value when the mouse wheel is scrolled up', function  (){
+        it('should increase the value when the mouse wheel is scrolled up', function  (){
             changeInputValueTo(element, '1')
             triggerMouseWheelEvent(element, 1);
-            expect(element.val()).toEqual('2');
+            expect(getElementValue(element)).toEqual('2');
             expect($scope.input).toEqual(2);
 
             triggerMouseWheelEvent(element, 1);
-            expect(element.val()).toEqual('3');
+            expect(getElementValue(element)).toEqual('3');
             expect($scope.input).toEqual(3);
         });
 
-        it('by decreasing the value when the mouse wheel is scrolled down', function () {
+        it('should decrease the value when the mouse wheel is scrolled down', function () {
             changeInputValueTo(element, '2');
             triggerMouseWheelEvent(element, -1);
-            expect(element.val()).toEqual('1');
+            expect(getElementValue(element)).toEqual('1');
             expect($scope.input).toEqual(1);
 
             triggerMouseWheelEvent(element, -1);
-            expect(element.val()).toEqual('0');
+            expect(getElementValue(element)).toEqual('0');
             expect($scope.input).toEqual(0);
         });
 
-        it('by starting at zero when the mouse wheel is scrolled up for an empty value', function () {
+        it('should start at zero when the mouse wheel is scrolled up for an empty value', function () {
             changeInputValueTo(element, '');
             triggerMouseWheelEvent(element, 1);
-            expect(element.val()).toEqual('1');
+            expect(getElementValue(element)).toEqual('1');
             expect($scope.input).toEqual(1);
         });
 
-        it('by starting at zero when the mouse wheel is scrolled down for an empty value', function () {
+        it('should start at zero when the mouse wheel is scrolled down for an empty value', function () {
             changeInputValueTo(element, '');
             triggerMouseWheelEvent(element, -1);
-            expect(element.val()).toEqual('-1');
+            expect(getElementValue(element)).toEqual('-1');
             expect($scope.input).toEqual(-1);
         });
     });
@@ -247,22 +263,22 @@ describe('numberinput', function () {
             it('for an increment', function () {
                 changeInputValueTo(element, '1');
                 triggerKeyDown(element, 38);
-                expect(element.val()).toEqual('2');
+                expect(getElementValue(element)).toEqual('2');
                 expect($scope.input).toEqual(2);
 
                 triggerMouseWheelEvent(element, 1);
-                expect(element.val()).toEqual('3');
+                expect(getElementValue(element)).toEqual('3');
                 expect($scope.input).toEqual(3);
             });
 
             it('for a decrement', function () {
                 changeInputValueTo(element, '2');
                 triggerKeyDown(element, 40);
-                expect(element.val()).toEqual('1');
+                expect(getElementValue(element)).toEqual('1');
                 expect($scope.input).toEqual(1);
 
                 triggerMouseWheelEvent(element, -1);
-                expect(element.val()).toEqual('0');
+                expect(getElementValue(element)).toEqual('0');
                 expect($scope.input).toEqual(0);
             });
         });
@@ -277,22 +293,22 @@ describe('numberinput', function () {
             it('for an increment', function () {
                 changeInputValueTo(element, '0');
                 triggerKeyDown(element, 38);
-                expect(element.val()).toEqual('10');
+                expect(getElementValue(element)).toEqual('10');
                 expect($scope.input).toEqual(10);
 
                 triggerMouseWheelEvent(element, 1);
-                expect(element.val()).toEqual('20');
+                expect(getElementValue(element)).toEqual('20');
                 expect($scope.input).toEqual(20);
             });
 
-            it('for an decrement', function () {
+            it('for a decrement', function () {
                 changeInputValueTo(element, '50');
                 triggerKeyDown(element, 40);
-                expect(element.val()).toEqual('40');
+                expect(getElementValue(element)).toEqual('40');
                 expect($scope.input).toEqual(40);
 
                 triggerMouseWheelEvent(element, -1);
-                expect(element.val()).toEqual('30');
+                expect(getElementValue(element)).toEqual('30');
                 expect($scope.input).toEqual(30);
             });
         });
@@ -305,12 +321,12 @@ describe('numberinput', function () {
 
                 changeInputValueTo(element, '0.06');
                 triggerKeyDown(element, 38);
-                expect(element.val()).toEqual('0.07'); // can be 0.06999999999999...round properly!
+                expect(getElementValue(element)).toEqual('0.07'); // can be 0.06999999999999...round properly!
                 expect($scope.input).toEqual(0.07);
 
                 changeInputValueTo(element, '0.06');
                 triggerMouseWheelEvent(element, 1);
-                expect(element.val()).toEqual('0.07');
+                expect(getElementValue(element)).toEqual('0.07');
                 expect($scope.input).toEqual(0.07);
             });
         });
@@ -321,7 +337,7 @@ describe('numberinput', function () {
                var element = createInput('<numberinput ng-model="input" step="abc" />');
                 changeInputValueTo(element, '1.25');
                 triggerKeyDown(element, 38);
-                expect(element.val()).toEqual('2.25');
+                expect(getElementValue(element)).toEqual('2.25');
                 expect($scope.input).toEqual(2.25);
             });
 
@@ -329,7 +345,7 @@ describe('numberinput', function () {
                 var element = createInput('<numberinput ng-model="input" decimals="2" step="0.001" />');
                 changeInputValueTo(element, '1.25');
                 triggerKeyDown(element, 38);
-                expect(element.val()).toEqual('1.26');
+                expect(getElementValue(element)).toEqual('1.26');
                 expect($scope.input).toEqual(1.26);
             });
         });
