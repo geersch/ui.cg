a185cabfefe5c937b792479bdd72a2b395bdd65f
Christophe Geers <geersch@gmail.com>
2018-04-02 18:19:32 +0200

    Updates

--- js/docs-setup.js
+++ js/docs-setup.js
@@ -10,7 +10,8 @@ NG_DOCS={
       "type": "overview",
       "moduleName": "Overview",
       "shortDescription": "Input controls for your Angular apps, made easy",
-      "keywords": "add adipiscing aliquet amet angular angularjs api apps blandit bring build built cg clone configurable consectetur controls decimal decimals dependencies dictum dist dolor easiest easy easy-to-use elit enim erat eros faucibus fermentum feugiat find floating folder fringilla geers git grunt imperdiet input install ipsum javascript justo lorem module mymodule nec nisl nmp node npm nulla number numberinput org overview point porttitor quick quis quisque redistributables repository rutrum separator sit start tempus turpis ui ultrices uncompressed ut vel vestibulum vivamus"
+      "keywords": "add adipiscing aliquet amet angular angularjs api apps blandit bring build built cg clone configurable consectetur controls decimal decimals dependencies dictum dist dolor easiest easy easy-to-use elit enim erat eros faucibus fermentum feugiat find floating folder fringilla geers git grunt imperdiet input install ipsum javascript justo lorem module mymodule nec nisl nmp node npm nulla number numberinput org overview point porttitor quick quis quisque redistributables repository rutrum separator sit start tempus turpis ui ultrices uncompressed ut vel vestibulum vivamus",
+      "isDeprecated": false
     },
     {
       "section": "api",
@@ -19,7 +20,8 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "cg.ui",
       "shortDescription": "Add this attribute on an input element so that it knows whether or not it has focus.",
-      "keywords": "$focused $scope add angular api app attribute cg controller directive element false focus focused form formname function html input input1 input2 input3 inputname js mainctrl module ng-controller ng-model number numberinput returns text true type ui value1 value2 value3 var"
+      "keywords": "$focused $scope add angular api app attribute cg controller directive element false focus focused form formname function html input input1 input2 input3 inputname js mainctrl module ng-controller ng-model number numberinput returns text true type ui value1 value2 value3 var",
+      "isDeprecated": false
     },
     {
       "section": "api",
@@ -28,16 +30,8 @@ NG_DOCS={
       "type": "directive",
       "moduleName": "cg.ui",
       "shortDescription": "Input for a floating point number with a configurable decimal separator (default: .) and decimals (default: 2).",
-      "keywords": "$scope allowed angular api app arrow arrows assignable button-descr buttons cg checkbox class configurable controller controls data-bind decimal decimal-separator decimals decrease decrement directive disable disabled display expression false floating function html increase increment input inside js key-button keyboard keyboard-legend legend maximium maximum minimum module mousewheel ng-controller ng-disabled ng-model ngdisabled ngmodel number numberinput numberinputctrl point scroll separator spin spinner step true type ui user var"
-    },
-    {
-      "section": "api",
-      "id": "cg.ui.directive:timepicker",
-      "shortName": "timepicker",
-      "type": "directive",
-      "moduleName": "cg.ui",
-      "shortDescription": "A simple timepicker.",
-      "keywords": "$scope angular api app cg controller directive function html js module ng-controller ng-model simple time timepicker timepickerctrl ui var"
+      "keywords": "$scope allowed angular api app arrow arrows assignable button-descr buttons cg checkbox class configurable controller controls data-bind decimal decimal-separator decimals decrease decrement directive disable disabled display expression false floating function html increase increment input inside js key-button keyboard keyboard-legend legend maximium maximum minimum module mousewheel ng-controller ng-disabled ng-model ngdisabled ngmodel number numberinput numberinputctrl point scroll separator spin spinner step true type ui user var",
+      "isDeprecated": false
     }
   ],
   "apis": {
